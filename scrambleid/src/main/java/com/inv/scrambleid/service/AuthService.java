128c1f4214424d808e7c2b3722bd90a57ef2254b
inv-abdul-fahad <abdul.fahad@innovaturelabs.com>
2026-05-07 17:44:04 +0530

    login response changed

--- scrambleid/src/main/java/com/inv/scrambleid/service/AuthService.java
+++ scrambleid/src/main/java/com/inv/scrambleid/service/AuthService.java
@@ -3,10 +3,11 @@ package com.inv.scrambleid.service;
 
 import com.inv.scrambleid.forms.LoginRequest;
 import com.inv.scrambleid.forms.RegisterRequest;
+import com.inv.scrambleid.view.LoginResponse;
 
 public interface AuthService {
 
     String register(RegisterRequest request);
 
-    String login(LoginRequest request);
+    LoginResponse login(LoginRequest request);
 }
